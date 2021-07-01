9dea0c4bf959c3dd0eb500282cc9568300514007
cctorres <c.torresalzate@gmail.com>
2021-07-01 08:19:25 -0500

    initial commit

--- src/Components/Pages/Perfiles.js
+++ src/Components/Pages/Perfiles.js
@@ -3,12 +3,47 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./Perfiles.css";
 
 const Perfiles = () => {
+  const megumi =
+    "https://www.otakulegion.com/wp-content/uploads/2021/04/5c3768388305bf480f93b7511027ac3b-1024x1024.jpg";
+
   return (
-    <div className="perfiles">
+    <div classNameName="perfiles">
       <Container>
         <Row>
-          <Col><img className="card-img" src="https://www.otakulegion.com/wp-content/uploads/2021/04/5c3768388305bf480f93b7511027ac3b-1024x1024.jpg"></img></Col>
-          <Col><h1>Megumi</h1><p>Personaje de Jujutsu Kaisen</p></Col>
+          <Col>
+            <img className="img-card" src={megumi} />
+          </Col>
+          <Col>
+            Jujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu Kaisen
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Jujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu KaisenJujutsu KaisenJujutsu
+            KaisenJujutsu KaisenJujutsu Kaisen
+          </Col>
+          <Col>
+            <img className="img-card" src={megumi} />
+          </Col>
         </Row>
       </Container>
     </div>
